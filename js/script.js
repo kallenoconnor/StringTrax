e37a2e054c03b1522335ce49a6a04e115ce3c200
Kallen O'Connor <kallenbierly@gmail.com>
2023-05-12 16:07:12 -0400

    Tweaks to form and messing with safari iPhone styling (again)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//intersection observer for animatin text at certain page points
 const observerCheckHit = (/**@type {IntersectionObserverEntry}*/element )=>
 {
     if (element.isIntersecting)
@@ -8,3 +9,20 @@ const observerCheckHit = (/**@type {IntersectionObserverEntry}*/element )=>
 
 const observer = new IntersectionObserver( (e)=>e.forEach(observerCheckHit), { root: null, rootMargin: '0px', threshold: 0.1 } );
 document.querySelectorAll("[data-anim]").forEach(anim=>observer.observe(anim));
+
+//Form manipulation (prevent default redirect)
+const form = document.getElementById("contactForm");
+form?.addEventListener("submit", e=>
+{
+    e.preventDefault();
+
+    //Get form data
+    var data= new FormData(form);
+    //Init fetch post
+    fetch(form.getAttribute("action"), {
+        method: "POST",
+        body: data
+    })
+    //Server Response
+    .then(()=>document.body.setAttribute("class", "done"))
+});
